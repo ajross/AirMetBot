ae27b93573256b0f49e6e6763a041e2dd6400c01
Alasdair Ross <Alasdair.Ross@gmail.com>
2018-05-12 22:37:15 +0100

    Add a Hello World route to see if it's up

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ from AirMetBot import AirMetBot
 
 application = Flask(__name__)
 
+application.add_url_rule('/', 'index', (lambda: "Hello World!"))
+
 airmetbot = AirMetBot()
 
 if __name__ == "__main__":
